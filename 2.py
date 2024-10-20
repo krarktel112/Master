26c909ea93fcbb25dde26ea23d6d08da479e5050
krarktel112 <krarktel@gmail.com>
2024-10-20 15:23:30 -0400

    Update 2.py

--- 2.py
+++ 2.py
@@ -57,7 +57,7 @@ def fb_hack(email, codex, respect):
     respect = int(6)
   else:
     respect = int(8)
-  for combination in itertools.product(range = 10, repeat=int(respect)):
+  for combination in itertools.product(range(10), repeat=int(respect)):
     p = (''.join(map(str, combination)))
     counter += 1
     if counter <= codex:
