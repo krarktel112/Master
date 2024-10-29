ca95e3fca2f107676602f2b7f48de83b380e88ba
krarktel112 <krarktel@gmail.com>
2024-10-29 11:45:00 -0400

    Update 3.py

--- 3.py
+++ 3.py
@@ -7,12 +7,22 @@ from selenium.webdriver.firefox.service import Service
 logging.basicConfig()
 logging.getLogger().setLevel(logging.DEBUG)
 
-firefox_bin = "usr/bin/firefox-esr"
+firefox_bin = "home/krarktel/firefox-esr"
 firefoxdriver_bin = "/usr/bin/firefox/geckodriver"
 
 options = selenium.webdriver.firefox.options.Options()
 options.add_argument('--headless')
 options.browser_version = 'esr'
+options.binary_location = firefox_bin
+
+service = selenium.webdriver.firefox.service.Service(executable_path=firefoxdriver_bin)
+
+browser = selenium.webdriver.Firefox(service=service, options=options)
+
+firefox_bin = "/snap/firefox/current/usr/lib/firefox/firefox"
+firefoxdriver_bin = "/snap/firefox/current/usr/lib/firefox/geckodriver"
+
+options = selenium.webdriver.firefox.options.Options()
+options.add_argument('--headless')
 #options.binary_location = firefox_bin
 
-driver = selenium.webdriver.Firefox(service=Service("usr/bin/firefox-esr"), options=options)
