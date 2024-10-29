fdbb47d2616da5a2dd2abf1209e52d0312d34dad
krarktel112 <krarktel@gmail.com>
2024-10-29 12:22:50 -0400

    Update 3.py

--- 3.py
+++ 3.py
@@ -21,10 +21,7 @@ Firefox = "firefox-esr"
 
 browser = selenium.webdriver.Firefox(options=options)
 
-firefox_bin = "/snap/firefox/current/usr/lib/firefox/firefox"
-firefoxdriver_bin = "/snap/firefox/current/usr/lib/firefox/geckodriver"
-
-options = selenium.webdriver.firefox.options.Options()
-options.add_argument('--headless')
-#options.binary_location = firefox_bin
+logging.basicConfig()
+logging.getLogger().setLevel(logging.DEBUG)
+logger = logging.getLogger('selenium')
 
