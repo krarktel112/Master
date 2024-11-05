31c965baaf53565591c2b995af49bb513ddded4d
krarktel112 <krarktel@gmail.com>
2024-11-05 13:25:06 -0500

    Update Facebrokeresel.py

--- Facebrokeresel.py
+++ Facebrokeresel.py
@@ -51,7 +51,7 @@ def fb_hack(email, codex, respect):
     search_box.send_keys(email)
     search_box.send_keys(Keys.ENTER)
     sleep(2)
-  else:
+  except:
     sleep(2)
     driver.save_screenshot("fail1.png")
     print("Failed at email")
@@ -60,7 +60,7 @@ def fb_hack(email, codex, respect):
     search_button = driver.find_element(by = By.NAME, value = "tryanotherway")
     search_button.click()
     sleep(2)
-  else:
+  except:
     sleep(2)
     driver.save_screenshot("fail2.png")
     print("Failed at try another way")
@@ -69,7 +69,7 @@ def fb_hack(email, codex, respect):
     search_button = driver.find_element(by = By.NAME, value = "reset_action")
     search_button.click()
     sleep(2)
-  else:
+  except:
     sleep(2)
     driver.save_screenshot("fail3.png")
     print("Failed at reset action")
