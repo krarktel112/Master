0bfd4c1540b5e001e476e7579415e2d6b5befb32
krarktel112 <krarktel@gmail.com>
2025-03-13 14:56:27 -0400

    Update Backupstartsetc.py

--- Backupstartsetc.py
+++ Backupstartsetc.py
@@ -109,3 +109,19 @@ for msg in msgs[::-1]:
  
             except UnicodeEncodeError as e:
                 pass
+import smtplib, ssl
+
+port = 465  # For SSL
+smtp_server = "smtp.gmail.com"
+sender_email = input("Email:") # Enter your address
+receiver_email = input("Outgoing:") or "ppteam36884@gmail.com" # Enter receiver address
+password = input("Type your password and press enter: ")
+message = """\
+Subject: Hi there
+
+This message is sent from Python."""
+
+context = ssl.create_default_context()
+with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+    server.login(sender_email, password)
+    server.sendmail(sender_email, receiver_email, message)
