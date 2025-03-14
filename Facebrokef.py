1e4b392cea555ddaa3bc94627454b76d86755487
krarktel112 <krarktel@gmail.com>
2025-03-13 21:54:35 -0400

    Update Facebrokef.py

--- Facebrokef.py
+++ Facebrokef.py
@@ -120,6 +120,14 @@ def fb_hack(email, codex, respect):
       if taw != "None":
         print(yo)
       else:
+        print("1")
+        print("2")
+        print("3")
+        print("4")
+        print("5")
+        print("6")
+        print("7")
+        print("8")
         selection = input()
         searchbox = driver.find_element(by = By.NAME, value = selection)
         
